d874de509378a044b8881970c3c36ce442744488
Joy Lin <vvlnote@hotmail.com>
2019-07-29 18:58:21 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker {
+  constructor() {
+    super();
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  handleClick = () => {
+    const count = this.state.timeClicked;
+    this.setState({
+      timesClicked: count + 1
+    })
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+    )
+  }
+}
